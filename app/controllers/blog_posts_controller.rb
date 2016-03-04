973c84dabdfa0a015a27c8979a61f5bb42b4915f
Shubham Gupta <shubhamgupta0122@hotmail.com>
2016-03-04 22:18:50 +0530

    removed rating params and fields

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -69,6 +69,6 @@ class BlogPostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_post_params
-      params.require(:blog_post).permit(:title, :description, :author, :rating)
+      params.require(:blog_post).permit(:title, :description, :author)
     end
 end
